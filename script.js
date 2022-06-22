1a43106b9c4c95f60c5e9df50cdb04e55134d8e9
Jayden Richardson <jsrich@hotmail.co.nz>
2022-06-22 22:00:06 +1200

    Adjust code layout to make prettier

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ Write a function called add7 that takes one number and returns that number + 7
 */
 
 function add7(num1, num2, result) {
-  num1 = document.getElementsByClassName("add7Input")[0].value;
-  num2 = parseInt(num1) + 7;
-  result = document.getElementById("add7Result");
-  result.innerText = num2;
+    num1 = document.getElementsByClassName("add7Input")[0].value;
+    num2 = parseInt(num1) + 7;
+    result = document.getElementById("add7Result");
+    result.innerText = num2;
 }
 
 /*
@@ -14,11 +14,11 @@ Write a function called multiply that takes 2 numbers and returns their product.
 */
 
 function multiply(num1, num2, num3, result) {
-  num1 = document.getElementsByClassName("multiplyInput1")[0].value;
-  num2 = document.getElementsByClassName("multiplyInput2")[0].value;
-  num3 = parseInt(num1) * parseInt(num2);
-  result = document.getElementById("multiplyResult");
-  result.innerText = num3;
+    num1 = document.getElementsByClassName("multiplyInput1")[0].value;
+    num2 = document.getElementsByClassName("multiplyInput2")[0].value;
+    num3 = parseInt(num1) * parseInt(num2);
+    result = document.getElementById("multiplyResult");
+    result.innerText = num3;
 }
 
 /*
@@ -28,9 +28,10 @@ Write a function called capitalize that takes a string and returns that string w
 function capitalize(str, result) {
     str = document.getElementsByClassName("capitalizeInput")[0].value;
     result = document.getElementById("capitalizeOutput");
-    result.innerText = str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
+    result.innerText =
+        str.charAt(0).toUpperCase() + str.substring(1).toLowerCase();
 }
-  /*
+/*
     Below was a test of the capitalize function.. no idea what i was doing.
 
   firstLetter = str.charAt(0);
@@ -58,7 +59,7 @@ Write a function called lastLetter that takes a string and returns the very last
 */
 
 function lastLetter(str, result) {
-  str = document.getElementsByClassName("lastLetterInput")[0].value;
-  result = document.getElementById("lastLetterResult");
-  result.innerText = str.charAt(str.length - 1);
+    str = document.getElementsByClassName("lastLetterInput")[0].value;
+    result = document.getElementById("lastLetterResult");
+    result.innerText = str.charAt(str.length - 1);
 }
